c6538834b265241f8e51ae5220cfd4b2b2a23c76
Anilkumar463 <anilkumargm2005@gmail.com>
2026-04-16 07:13:43 +0000

    first commit

---
+++ src/test/java/com/example/AppTest.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppTest {
+
+    @Test
+    void testAppRuns() {
+        assertTrue(true, "This test always passes");
+    }
+}
